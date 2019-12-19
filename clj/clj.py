932fa916e0b58aa8f9b5fd44c52a37e173009a85
Erawpalassalg <mael.arnaud@gmail.com>
2019-12-19 20:45:18 +0100

    Remove command group
    Now only use `clj` as a keyword.
    
    The `clj task` subcommand has been removed since we do not want to
    support any other subcommand, after all.

--- clj/clj.py
+++ clj/clj.py
@@ -1,55 +1,138 @@
 """Main file for the clj commands group"""
+import copy
 import shutil
 
+from enum import Enum, unique
 from datetime import datetime
 from pathlib import Path
 
 import click
 
-from .commands import task  # pylint: disable=relative-beyond-top-level
+@unique
+class Status(Enum):
+    """Task possible status"""
+
+    PENDING = "•"
+    DONE = "v"
+    POSTPONED = ">"
+    ERASED = "x"
+
+    def int_val(self):
+        """Int value for sorting purposes"""
+        return {"•": 1, ">": 2, "v": 3, "x": 4}[self.value]
+
+    def __repr__(self):
+        return self.value
+
+    def __str__(self):
+        return self.__repr__()
 
 
-@click.group()
-@click.pass_context
-def cli(ctx):
-    """clj main command groups"""
-    journal_path = Path.home() / ".clj"
+def load_tasks(page_path: Path) -> list():
+    """Extract the tasks from a file path"""
+    if not page_path.exists():
+        return []
+
+    with page_path.open("r", encoding="utf-8") as file_:
+        return [
+            {"status": Status(line.strip()[0]), "task": line.strip()[1:]} for line in file_
+        ]
+
+
+def store_tasks(page_path: Path, tasks) -> None:
+    """Save the current tasks in a file"""
+    with page_path.open("w", encoding="utf-8") as file_:
+        file_.write("\n".join(["".join([str(v) for v in task.values()]) for task in tasks]))
+
+
+def postpone_pending(previous_page_path: Path, current_page_path: Path) -> None:
+    """Mark the PENDING tasks in the previous file as POSTPONED and append them to the set of
+    current tasks as PENDING
+
+    """
+    postponed_tasks = []
+
+    tasks = load_tasks(previous_page_path)
+    for task in tasks:
+        if task["status"] is Status.PENDING:
+            task["status"] = Status.POSTPONED
+
+        if task["status"] is Status.POSTPONED:
+            new_task = copy.deepcopy(task)
+            new_task["status"] = Status.PENDING
+            postponed_tasks.append(new_task)
+
+    store_tasks(previous_page_path, tasks)
+
+    tasks = load_tasks(current_page_path) + postponed_tasks
+    store_tasks(current_page_path, tasks)
+
+
+BOLD = "\033[1m"
+END = "\033[0m"
+
+
+@click.command()
+@click.option("--all/--no-all", "all_", default=False)
+@click.option("-a", "--add", type=str, multiple=True)
+@click.option("-x", "--cross", type=int, multiple=True)
+@click.option("-r", "--restore", type=int, multiple=True)
+@click.option("-v", "--check", type=int, multiple=True)
+def cli(all_, add, cross, check, restore): # pylint: disable=too-many-arguments
+    """Task subcommand for clj group"""
+    journal_path = Path.home() / ".clj" / "default"
 
     archive_path = journal_path / "archives"
     archive_path.mkdir(exist_ok=True, parents=True)
 
-    current_page_path = journal_path / str(datetime.date(datetime.now()))
-    current_page_path.mkdir(exist_ok=True, parents=True)
+    current_page_path = journal_path / f"{datetime.date(datetime.now())}.txt"
 
-    folders = list(journal_path.glob("*"))
-    folders.remove(archive_path)
-    folders.remove(current_page_path)
+    pages = list(journal_path.glob("*.txt"))
 
-    if len(folders) > 1:
-        raise RuntimeError("Journal directory is corrupted")
+    if pages:
+        if len(pages) > 1:
+            raise RuntimeError("Journal directory is corrupted")
 
-    if folders:
-        folder = folders[0]
+        if pages[0] != current_page_path:
+            postpone_pending(pages[0], current_page_path)
+            shutil.move(str(pages[0]), str(archive_path))
 
-        # Postpone tasks
-        previous_task_file = task.get_task_file(folder)
-        current_task_file = task.get_task_file(current_page_path, create=True)
+    tasks = load_tasks(current_page_path)
 
-        task.postpone_pending(previous_task_file, current_task_file)
+    for idx in restore:
+        try:
+            tasks[idx]["status"] = Status.PENDING
+        except IndexError:
+            print(f"Task n° {idx} does not exist")
 
-        # Archive previous journal page
-        shutil.move(str(folder), str(archive_path))
+    for idx in cross:
+        try:
+            tasks[idx]["status"] = Status.ERASED
+        except IndexError:
+            print(f"Task n° {idx} does not exist")
 
-    ctx.obj = {
-        "journal": journal_path,
-        "page": current_page_path,
-        "archive": archive_path,
-    }
+    for idx in check:
+        try:
+            tasks[idx]["status"] = Status.DONE
+        except IndexError:
+            print(f"Task n° {idx} does not exist")
+
+    for task in add:
+        tasks.append({"status": Status.PENDING, "task": task})
+
+    tasks = sorted(tasks, key=lambda task: task["status"].int_val())
+
+    store_tasks(current_page_path, tasks)
+
+    tasks = [t for t in tasks if all_ or t["status"] is Status.PENDING]
+
+    for idx, task in enumerate(tasks):
+        task = copy.deepcopy(task)
+        print(f"{idx} {BOLD}{str(task['status'])}{END} {task['task']}")
 
 
 def main():
     """Entry point for poetry build"""
-    cli.add_command(task.cli)
     cli()  # pylint: disable=no-value-for-parameter
 
 
